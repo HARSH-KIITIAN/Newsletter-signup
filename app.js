d8e75bc21a49b74d82c6e54c35434d957d750e40
Harsh Kumar <harsh84090kumar@gmail.com>
2023-06-20 13:32:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.post("/", (req,res)=>{
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = process.env.URL;
+
+    const options = {
+        method: "POST",
+        auth: process.env.AUTH
+    }
+
+    const request = https.request(url, options, (response)=>{
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname+"/success.html");
+        }else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/failure", (req,res)=>{
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT, ()=>{
+    
+})
